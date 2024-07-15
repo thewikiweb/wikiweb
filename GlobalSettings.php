8b28cf9ece5cc2f02721dd3841acb7e9c716c7f5
rickbot dot <115214762+rickbot-dot@users.noreply.github.com>
2024-07-15 22:31:29 +0100

    Update GlobalSettings.php

--- GlobalSettings.php
+++ GlobalSettings.php
@@ -1,11 +1,20 @@
 <?php
 
+/*
+
+This is a file containing settings for all wikiweb wikis.
+This file is made specifically for wikiweb and may require
+adjustments to work on other farms.
+
+SPDX-License-Identifier: GPL-3.0-or-later
+
+*/
+
+// If this isn't MediaWiki, stop
+
 if ( !defined( 'MEDIAWIKI' ) ) {
         exit;
 }
-
-$wgEnableUploads = false;
-$wgUseImageMagick = false;
 
 // Global Extensions
 wfLoadExtension( 'Echo' ); // Needed for LoginNotify.
@@ -14,9 +23,11 @@ wfLoadExtension( 'Interwiki' ); // Core MediaWiki interface.
 wfLoadExtension( 'WikiEditor' ); // Core MediaWiki interface.
 wfLoadExtension( 'Scribunto' ); // Core MediaWiki interface.
 wfLoadExtension( 'SecureLinkFixer' ); // Ensure everybody uses HTTPS.
+// wfLoadExtension( 'NewWiki' ); // This is in development so not available yet.
+// wfLoadExtension( 'WWConsole' ); // Same thing.
 wfLoadSkin( 'MonoBook' );
 wfLoadSkin( 'Vector' );
 
 // Load database, this file isn't on the repository and only available on
 // the main WikiWeb servers.
-include("DatabaseSettings.php");
+include("/var/www/DatabaseSettings.php");
